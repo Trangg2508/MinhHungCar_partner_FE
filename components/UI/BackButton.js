248cae7d17cbbf943a68b7c2de575b3050f2fb93
eung-eung <boyvip5231@gmail.com>
2024-06-25 07:20:24 +0700

    build back button component

---
+++ components/UI/BackButton.js
@@ -0,0 +1,22 @@
+import { Alert, Button, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+export default function BackButton({ title, subTitle, callBack }) {
+    const navigate = useNavigation()
+    return (
+        <View>
+            <Button onPress={() => {
+                Alert.alert(title,
+                    subTitle
+                    ,
+                    [
+                        { text: "Ở lại", onPress: () => { } },
+                        { text: "Hủy", style: "destructive", onPress: callBack }
+                    ]
+                )
+            }} style={{ color: "blue" }} title='Trở về' />
+        </View>
+    )
+
+}
